36b350db25bb2affe6ef797e00c9ef96fe8e1b8a
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-09-28 09:07:54 -0400

    Refactor code to use Axios instead.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const yargs = require('yargs');
-
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
 
 // Object that stores the final parsed output
 // It takes the input from the process variable and pass it through yargs
@@ -18,25 +16,43 @@ const argv = yargs
   .help()
   .alias('help', 'h').argv; // 1st arg the actual agrument, 2nd arg is alias
 
-// Call function responsible for all the logic behind geolocation, and call back with either an error message/results
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(results.address);
-    // Dynamically pass the longitude and latitude to the weather search function
-    weather.getWeather(
-      results.latitude,
-      results.longitude,
-      (errorMessage, weatherResults) => {
-        if (errorMessage) {
-          console.log(errorMessage);
-        } else {
-          console.log(
-            `It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`
-          );
-        }
-      }
+// Replace spaces etc. for URI components to inject into URL
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+// axios library knows how to parse JSON data automatically without a setting for it
+// .get returns a promise, so you can use .then to run code when a promise gets fullfilled/rejected
+axios
+  .get(geocodeURL)
+  .then(response => {
+    // In the response schema of the axios library, "data" is the response that was provided by the server
+    if (response.data.status === 'ZERO_RESULTS') {
+      // If the status ZERO_RESULTS is returned skip to the catch else statement below
+      throw new Error('Unable to find that address.');
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/254caa042e5270830fcd5f2f3f4a8bda/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    // Make a second call by returning a new promise
+    return axios.get(weatherUrl);
+  }) // When weather data arrives...
+  .then(response => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(
+      `It's currently ${temperature}. It feels like ${apparentTemperature}.`
     );
-  }
-});
+  })
+  .catch(e => {
+    if (
+      e.code === 'ECONNREFUSED' ||
+      e.code === 'ENOTFOUND' ||
+      e.code === 'ETIMEOUT'
+    ) {
+      console.log('Unable to connect to API servers/');
+    } else {
+      // Print the Error constructor message from earlier which is property name of "message"
+      console.log(e.message);
+    }
+  });
